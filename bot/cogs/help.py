d7c9f205dea1abaf8aa84cd85a1620c21aa81164
MrNed21 <60824911+MrNed21@users.noreply.github.com>
2022-03-26 18:44:34 -0400

    Update help.py

--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -7,16 +7,13 @@ class Help(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    async def help(self, mapping):
-        embed = discord.Embed(title="Help")
-        for cog, commands in mapping.items():
-           command_signatures = [
-               self.get_command_signature(c) for c in commands]
-           if command_signatures:
-               cog_name = getattr(cog, "qualified_name", "No Category")
-               embed.add_field(name=cog_name, value="\n".join(
-                    command_signatures), inline=False)
+    commands.command()
+    async def send_command_help(self, command):
+        embed = discord.Embed(title=self.get_command_signature(command))
+        embed.add_field(name="Help", value=command.help)
+        alias = command.aliases
+        if alias:
+            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
 
         channel = self.get_destination()
         await channel.send(embed=embed)
