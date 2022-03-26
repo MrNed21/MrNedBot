0fb0a4e4706667358338b2832615f8367d2450a5
MrNed21 <60824911+MrNed21@users.noreply.github.com>
2022-03-26 18:45:54 -0400

    Update help.py
    ugggg helpo

--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -7,13 +7,14 @@ class Help(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    commands.command()
-    async def send_command_help(self, command):
+    @commands.command()
+    async def help(self, command):
         embed = discord.Embed(title=self.get_command_signature(command))
         embed.add_field(name="Help", value=command.help)
         alias = command.aliases
         if alias:
-            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
+            embed.add_field(
+                name="Aliases", value=", ".join(alias), inline=False)
 
         channel = self.get_destination()
         await channel.send(embed=embed)
