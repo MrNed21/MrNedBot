d93f0858b3b44b50ec2a16864e925784d0771185
MrNed21 <60824911+MrNed21@users.noreply.github.com>
2022-03-26 18:55:31 -0400

    AAAAAAAAAA
    AAAAAAAAAA

--- bot/main.py
+++ bot/main.py
@@ -16,11 +16,38 @@ intents.members = True
 client = commands.Bot(command_prefix='ned ',
                       intents=intents)
 #client.remove_command('help')
+
+
 class MyHelp(commands.HelpCommand):
+    def get_command_signature(self, command):
+        return '%s%s %s' % (self.clean_prefix, command.qualified_name, command.signature)
     async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Help")
+        for cog, commands in mapping.items():
+           filtered = await self.filter_commands(commands, sort=True)
+           command_signatures = [self.get_command_signature(c) for c in filtered]
+           if command_signatures:
+                cog_name = getattr(cog, "qualified_name", "No Category")
+                embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
+
         channel = self.get_destination()
-        await channel.send("hello!")
-        
+        await channel.send(embed=embed)
+
+client.help_command = MyHelp()
+
+class MoreHelp(commands.MinimalHelpCommand):
+    async def send_command_help(self, command):
+        embed = discord.Embed(title=self.get_command_signature(command))
+        embed.add_field(name="Help", value=command.help)
+        alias = command.aliases
+        if alias:
+            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+client.help_command = MoreHelp()
+
 @tasks.loop()
 async def change_status():
     """This runs the task to loop these things in the status on discord"""
