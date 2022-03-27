b1ef25177dd1542a2e1e77151d815a54730d2db8
MrNed21 <60824911+MrNed21@users.noreply.github.com>
2022-03-27 10:21:35 -0400

    Update error.py

--- bot/cogs/error.py
+++ bot/cogs/error.py
@@ -11,14 +11,14 @@ class error(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        self.send_error(ctx, error)
+        ctx.send(embed=self.send_error(ctx, error))
 
     def send_error(self, ctx, error):
         embed = discord.Embed(title="Oh No! An error occured!",
                               description=error, color=0xff0000)
         embed.set_thumbnail(
             url="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclipground.com%2Fimages%2Fdisassemble-clipart-15.jpg&f=1&nofb=1")
-        await ctx.send(embed=embed)
+        return(embed)
 
 
 def setup(client):
