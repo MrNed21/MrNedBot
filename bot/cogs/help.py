17a9bb022396755f5c3ce3aa255b1ca5c040b358
MrNed21 <60824911+MrNed21@users.noreply.github.com>
2022-03-26 21:47:35 -0400

    Update help.py

--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -2,60 +2,6 @@ import discord
 from discord.ext import commands
 
 
-class MyHelp(commands.HelpCommand):
-    def get_command_signature(self, command):
-        return '%s%s %s' % (self.clean_prefix, command.qualified_name, command.signature)
-
-    async def send_bot_help(self, mapping):
-        embed = discord.Embed(title="Help", color=0x00b3ff)
-        for cog, commands in mapping.items():
-           filtered = await self.filter_commands(commands, sort=True)
-           command_signatures = [
-               self.get_command_signature(c) for c in filtered]
-           if command_signatures:
-               cog_name = getattr(cog, "qualified_name", "No Category")
-               embed.add_field(
-                   name=cog_name, value=f'ned help {cog_name}', inline=False)
-
-        channel = self.get_destination()
-        embed.set_thumbnail(
-            url="http://clipartmag.com/images/scroll-png-25.png")
-        embed.set_footer(
-                text="For specific parameters try ned help [command/catagory]")
-        await channel.send(embed=embed)
-
-    async def send_cog_help(self, cog):
-        self.client
-        embed = discord.Embed(
-            title=f"Help: {cog}", color=0x00b3ff)
-        for x in cog.commands:
-            embed.add_field(name=x, value=f'ned help {x}', inline=False)
-        channel = self.get_destination()
-        embed.set_thumbnail(
-            url="http://clipartmag.com/images/scroll-png-25.png")
-        embed.set_footer(
-                text="For specific parameters try ned help [command/catagory]")
-        await channel.send(embed=embed)
-
-    async def send_command_help(self, command):
-        embed = discord.Embed(
-            title=f"Help: {command}", color=0x00b3ff)
-        embed.add_field(
-            name='Usage', value=self.get_command_signature(command))
-        embed.add_field(name="Description", value=command.help)
-        alias = command.aliases
-        if alias:
-            embed.add_field(
-                name="Aliases", value=", ".join(alias), inline=False)
-
-        channel = self.get_destination()
-        embed.set_thumbnail(
-            url="http://clipartmag.com/images/scroll-png-25.png")
-        embed.set_footer(
-                text="For specific parameters try ned help [command/catagory]")
-        await channel.send(embed=embed)
-
-
 prefix = 'ned '
 embed_color = 0x00b3ff
 
@@ -100,8 +46,8 @@ class help(commands.Cog):
             for command in cog.get_commands():
                 embed.add_field(name=command, value=command.help, inline=False)
         #GET DETAILS FROM COMMAND
-        elif input in commands():
-            input=self.client.get_command(input)
+        elif input in commands:
+            input = self.client.get_command(input)
             embed = discord.Embed(
                 title="Help", description=f"{input}", color=embed_color)
 
