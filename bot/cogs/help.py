5603a20502ad27848ae3d25061ca5eba8366a2ca
MrNed21 <60824911+MrNed21@users.noreply.github.com>
2022-03-26 21:22:09 -0400

    Update help.py

--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -99,7 +99,7 @@ class help(commands.Cog):
             for command in cog.get_commands():
                 embed.add_field(name=command, value=command.help, inline=False)
         #GET DETAILS FROM COMMAND
-        elif input in self.client.walk_commands:
+        elif input in self.client.walk_commands():
             embed = discord.Embed(
                 title="Help", description=f"{input}", color=embed_color)
 
