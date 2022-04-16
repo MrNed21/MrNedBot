5af64257e1092144a4f3cac11d69c5e6f035a3bb
MrNed21 <60824911+MrNed21@users.noreply.github.com>
2022-04-16 08:37:37 -0400

    Update stocks.py

--- bot/cogs/stocks.py
+++ bot/cogs/stocks.py
@@ -26,7 +26,7 @@ class stock(commands.Cog, description='experimenting with stock stuff'):
         embed = discord.Embed()
         embed.add_field(name=symbol, value="1 year history", inline=False)
         file = discord.File(
-            r"MrNedBot/bot/assets/graph.png", filename="graph.png")
+            r"MrNedBot/bot/cogs/graph.png", filename="graph.png")
         embed.set_image(url="attachment://graph.png")
         await ctx.send(file=file, embed=embed)
 
