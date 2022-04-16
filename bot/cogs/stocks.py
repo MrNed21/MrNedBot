2e17ad73b5672496ae5b126b52657cc8dd4e6983
MrNed21 <60824911+MrNed21@users.noreply.github.com>
2022-04-16 08:54:46 -0400

    Update stocks.py

--- bot/cogs/stocks.py
+++ bot/cogs/stocks.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
-import yfinance
-import plotly.io as pio
-import plotly.graph_objects as go
+import plotly.graph_objs as go
+import yfinance as yf
 import os
 
 
@@ -15,23 +14,40 @@ class stock(commands.Cog, description='experimenting with stock stuff'):
     async def graph(self, ctx, symbol):
         symbol = symbol.upper()
 
-        ticker = yfinance.Ticker(symbol)
-        hist = ticker.history(period='1y')
-        fig = go.Figure(data=go.Scatter(
-            x=hist.index,
-            y=hist['Close'],
-            mode='lines+markers'))
+        data = yf.download(
+            tickers=symbol,
+            period='365d',
+            interval='15m',
+            rounding=bool)
 
-        pio.write_image(fig, "graph.png")
-        '''
+        fig = go.Figure()
+        fig.add_trace(go.Candlestick(
+            x=data.index, open=data['Open'], high=data['High'], low=data['Low'], close=data['Close'], name='market data'))
+        fig.update_layout(title=f' {symbol} share price',
+                          yaxis_title='Stock Price (USD)')
+        fig.update_xaxes(
+            rangeslider_visible=True,
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=15, label='15m',
+                         step="minute", stepmode="backward"),
+                    dict(count=45, label='45m',
+                         step="minute", stepmode="backward"),
+                    dict(count=1, label='1h', step="hour", stepmode="backward"),
+                    dict(count=6, label='6h', step="hour", stepmode="backward"),
+                    dict(step="all")
+                ])
+                )
+        )
+        fig.write_image("graph.png")
+
         embed = discord.Embed()
         embed.add_field(name=symbol, value="1 year history", inline=False)
         file = discord.File(
             r"MrNedBot/bot/cogs/graph.png", filename="graph.png")
         embed.set_image(url="attachment://graph.png")
         await ctx.send(file=file, embed=embed)
-        '''
-        await ctx.send('this command is literally broken')
+
         #os.remove("graph.png")
 
 
